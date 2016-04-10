23a24c630b4a0cf47d6625cd6ce74333797e598f
Jonathan Rach <jonathanrach@Jonathans-MacBook-Pro-4.local>
2016-04-10 06:24:32 -0400

    Fixed turn left method, uncommented parts for testing....
    IT WORKS!

--- Python/drone_demo.py
+++ Python/drone_demo.py
@@ -24,19 +24,32 @@ def _callback(message, channel):
     #print(message)
     if msg == "ret":
         print 'Drone returning home'
-        """drone.move(0,0,-.5,0) #reverse height increase
-        time.sleep(4)           #lower height for 4 seconds
+
+        drone.turnLeft()       #turn drone left to reverse direction
+        time.sleep(4.5)         #turn for 4.5 seconds to reverse direction
+
+        drone.moveForward()     #move drone forward
+        time.sleep(2)           #move forward for 2 seconds"""
+
+        drone.move(0,0,-1,0)   #reverse height increase
+        time.sleep(6)           #lower height for 4 seconds
 
         drone.land()
-        exit()"""
+        exit()
 
     elif msg != "" :
         print 'Drone leaving base'
-        """drone.takeoff()         #launch drone
+        drone.takeoff()         #launch drone
         time.sleep(7.5)         #wait 7.5s for drone to take off before executing commands
 
-        drone.move(0,0,.5,0)    #move drone up at half speed
-        time.sleep(4)           #move drone up for 4 seconds
+        drone.move(0,0,1,0)    #move drone up at half speed
+        time.sleep(6)           #move drone up for 4 seconds
+
+        """drone.turnRight(1)      #turn drone to the right at full speed
+        time.sleep(2.25)        #stop the turn after 90 degrees
+        """
+        drone.moveForward()
+        time.sleep(2)
 
         drone.hover()           #hover drone over drop site"""
 
